e8843ec722c8ad5a2a4bcead2cfb5a074a8f8ecd
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-07-05 16:22:03 +0200

    Edit function for best practice

--- script/main.js
+++ script/main.js
@@ -1,13 +1,11 @@
 let answerButton = document.querySelector('[data-js="answer-btn"]');
-
 let answer = document.querySelector('[data-js="answer-text"]');
+
 console.log(answerButton);
 console.log(answer);
 
-function showAnswer() {
+answerButton.addEventListener("click", () => {
   console.log("Button clicked!");
   answer.classList.toggle("answer-text--hidden");
   answerButton.textContent = "Hide answer";
-}
-
-answerButton.addEventListener("click", showAnswer);
+});
