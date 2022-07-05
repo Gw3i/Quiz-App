0f98f4bed3bdcfc53bf2226d50345af6873e5e5c
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-07-05 22:00:32 +0200

    Add change state of bookmark and show and hide button

--- script/main.js
+++ script/main.js
@@ -1,5 +1,8 @@
-let answerButton = document.querySelector('[data-js="answer-btn"]');
-let answer = document.querySelector('[data-js="answer-text"]');
+console.clear();
+
+// Show Answer Button //
+const answerButton = document.querySelector('[data-js="answer-btn"]');
+const answer = document.querySelector('[data-js="answer-text"]');
 
 console.log(answerButton);
 console.log(answer);
@@ -7,5 +10,21 @@ console.log(answer);
 answerButton.addEventListener("click", () => {
   console.log("Button clicked!");
   answer.classList.toggle("answer-text--hidden");
-  answerButton.textContent = "Hide answer";
+  if (answer.classList.contains("answer-text--hidden")) {
+    console.log("It works!");
+    answerButton.textContent = "Show answer";
+  } else {
+    console.log("That too!");
+    answerButton.textContent = "Hide answer";
+  }
+});
+
+// Fill Bookmark //
+
+const bookmarkButton = document.querySelector('[data-js="bookmark-button"');
+const bookmarkFilled = document.querySelector('[data-js="bookmark-filled"');
+
+bookmarkButton.addEventListener("click", () => {
+  console.log("Bookmark clicked!");
+  bookmarkFilled.classList.toggle("bookmark-icon--active");
 });
