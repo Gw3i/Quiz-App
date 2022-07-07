2aae6107fb1bda3543583688d8e719d12091c49f
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-07-08 01:11:45 +0200

    Add main.js file for show oages function

---
+++ components/main/main.js
@@ -0,0 +1,11 @@
+export default function switchPages() {
+    // Buttons
+    const bookmarkButton = document.querySelector('')
+
+   // Pages 
+  const bookmarkPage = document.querySelector('[data-js="bookmark-page"]');
+
+  let isShown = true;
+
+
+}
