d2ff50e51274c3376d006b4c43302447bd0cb809
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-07-08 00:59:33 +0200

    Add pages to index.hmtl and add function for dynamic card

--- components/question-card/card.js
+++ components/question-card/card.js
@@ -6,19 +6,41 @@ const showAnswer = QuestionCard();
 
 const questionCards = [
   {
-    titel: "Question:",
-    bookmark: bookmark,
     question: "What kind of animal is a Dolphin?",
-    answerButton: showAnswer,
     answer: "Dolphins are mammals, not fish",
     tags: ["animals", "sea", "fish", "biology", "sealife"],
   },
   {
-    titel: "Question:",
-    bookmark: bookmark,
     question: "What is the capitol of Japan?",
-    answerButton: showAnswer,
     answer: "Tokyo",
     tags: ["cities", "geography", "japan", "asia"],
   },
+  {
+    question: "What is the biggest building in the world?",
+    answer: "Warisan Merdeka Tower Skyscraper in Kuala Lumpur, Malaysia - 679m",
+    tags: ["cities", "geography", "japan", "asia"],
+  },
 ];
+
+questionCards.forEach((card) => {
+  // get card container
+  const cardContainer = document.querySelector('[data-js="card"]');
+  document.body.append(cardContainer);
+  // get question text and change question
+  const questionText = document.querySelector(
+    '[data-js="card__question-text"]'
+  );
+  questionText.innerText = card.question;
+
+  // get answer
+  const answer = document.querySelector('[data-js="answer-text"]');
+  answer.innerText = card.answer;
+
+  // get tags // just first tag is selected
+  const tagList = document.querySelector('[data-js="tag-list"]');
+  const tagListTag = document.querySelectorAll('[data-js="tag"]');
+  tagListTag.innerText = card.tags;
+  console.log(typeof tagList);
+});
+
+console.log(questionCards.length);
