07d79bf1ae87a534c93df658e1ca91d736e35a68
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-07-08 11:36:49 +0200

    Edit QuestionCard to apply samne logic on all cards

--- components/main/main.js
+++ components/main/main.js
@@ -1,11 +1,12 @@
 export default function switchPages() {
-    // Buttons
-    const bookmarkButton = document.querySelector('')
+  // Buttons
+  const bookmarkButton = document.querySelector('')
 
    // Pages 
   const bookmarkPage = document.querySelector('[data-js="bookmark-page"]');
 
-  let isShown = true;
-
+  let pageShown = true;
+    
+  
 
 }
--- script/index.js
+++ script/index.js
@@ -1,5 +1,5 @@
 import Bookmark from "../components/bookmarks/bookmarks.js";
-import QuestionCrad from "../components/question-card/question-card.js";
+import QuestionCard from "../components/question-card/question-card.js";
 
 Bookmark();
-QuestionCrad();
+QuestionCard();
