f7267600c35cc34bad7836563f49d0e43db79f12
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-07-10 19:37:06 +0200

    Create questions array

--- components/create/create.js
+++ components/create/create.js
@@ -1,1 +1,20 @@
-export default function Create() {}
+export default function Create() {
+  const questionCards = [
+    {
+      question: "What kind of animal is a Dolphin?",
+      answer: "Dolphins are mammals, not fish.",
+      tags: ["animals", "sea", "fish", "biology", "sealife"],
+    },
+    {
+      question: "What is the largest building in the world?",
+      answer: "Burj Khalifa in Dubia. 828m",
+      tags: ["architecture", "buildungs", "skyscrapper", "history", "mankind"],
+    },
+    {
+      question: "What is the capitol of France?",
+      answer: "Paris",
+      tags: ["geography", "europe", "france", "baguette", "croissants"],
+    },
+  ];
+}
+
