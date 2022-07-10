d342adda506d650a1ee4a171f1cd129f9ba926a5
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-07-10 19:19:28 +0200

    Add create.js

--- script/index.js
+++ script/index.js
@@ -2,8 +2,10 @@ import Bookmark from "../components/bookmarks/bookmarks.js";
 import QuestionCard from "../components/question-card/question-card.js";
 import CreateCards from "../components/question-card/card.js";
 import SwitchPages from "../components/nav/nav.js";
+import Create from "../components/create/create.js";
 
 Bookmark();
 QuestionCard();
 CreateCards();
 SwitchPages();
+Create();
