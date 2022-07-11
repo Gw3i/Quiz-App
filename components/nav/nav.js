3b91fe8df0098451b8a4d67bca888ca423fb75f3
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-07-11 12:45:34 +0200

    Change link classes to data-page

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -2,7 +2,7 @@ export default function SwitchPages() {
   console.clear();
 
   // Nav
-  const navList = document.querySelectorAll('[data-js="nav-list"]');
+  const navListItem = document.querySelectorAll('[data-js="nav-list"]');
   // Pages
   const homePage = document.querySelector('[data-js="home-page"]');
   const bookmarkPage = document.querySelector('[data-js="bookmark-page"]');
@@ -10,25 +10,13 @@ export default function SwitchPages() {
   const profilePage = document.querySelector('[data-js="profile-page"]');
 
   // Icons
-  const homeFilled = document.querySelector(
-    '[data-js="nav__home-icon--filled"]'
-  );
-  const bookmarkFilled = document.querySelector(
-    '[data-js="nav__bookmark-icon--filled"]'
-  );
-  const createFilled = document.querySelector(
-    '[data-js="nav__create-icon--filled"]'
-  );
-  const profileFilled = document.querySelector(
-    '[data-js="nav__profile-icon--filled"]'
-  );
 
   // Function
 
   // forEach
   const filledIcons = document.querySelectorAll('[data-js="icon--filled"]');
 
-  navList.forEach((navItem) => {
+  navListItem.forEach((navItem) => {
     const navButton = navItem.querySelector('[data-js="nav-button"]');
     const currentIcon = navItem.querySelector('[data-js="icon--filled"]');
 
@@ -37,23 +25,22 @@ export default function SwitchPages() {
       filledIcons.forEach((icon) => {
         icon.classList.add("icon--active");
       });
-      navList.forEach((navItem) => {
-        homePage.classList.add("page--hidden");
-        bookmarkPage.classList.add("page--hidden");
-        createPage.classList.add("page--hidden");
-        profilePage.classList.add("page--hidden");
-      });
+      homePage.classList.add("page--hidden");
+      bookmarkPage.classList.add("page--hidden");
+      createPage.classList.add("page--hidden");
+      profilePage.classList.add("page--hidden");
 
       // einblenden
       currentIcon.classList.remove("icon--active");
       // if homeButton clicked, show home page. If bookmarks clicked, show bookmarks etc.
-      if (navButton.classList.contains("bookmarksButton")) {
+      console.log(navButton.dataset.page);
+      if (navButton.dataset.page === "bookmarksButton") {
         bookmarkPage.classList.remove("page--hidden");
-      } else if (navButton.classList.contains("homeButton")) {
+      } else if (navButton.dataset.page === "homeButton") {
         homePage.classList.remove("page--hidden");
-      } else if (navButton.classList.contains("createButton")) {
+      } else if (navButton.dataset.page === "createButton") {
         createPage.classList.remove("page--hidden");
-      } else if (navButton.classList.contains("profileButton")) {
+      } else if (navButton.dataset.page === "profileButton") {
         profilePage.classList.remove("page--hidden");
       }
     });
