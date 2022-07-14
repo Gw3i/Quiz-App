93945308656d5c01c74d77f9de78d8dcce96bea3
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-07-14 15:23:02 +0200

    Add lintin

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -16,32 +16,32 @@ export default function SwitchPages() {
   // forEach
   const filledIcons = document.querySelectorAll('[data-js="icon--filled"]');
 
-  navListItem.forEach((navItem) => {
+  navListItem.forEach(navItem => {
     const navButton = navItem.querySelector('[data-js="nav-button"]');
     const currentIcon = navItem.querySelector('[data-js="icon--filled"]');
 
-    navButton.addEventListener("click", () => {
+    navButton.addEventListener('click', () => {
       // ausblenden
-      filledIcons.forEach((icon) => {
-        icon.classList.add("icon--active");
+      filledIcons.forEach(icon => {
+        icon.classList.add('icon--active');
       });
-      homePage.classList.add("page--hidden");
-      bookmarkPage.classList.add("page--hidden");
-      createPage.classList.add("page--hidden");
-      profilePage.classList.add("page--hidden");
+      homePage.classList.add('page--hidden');
+      bookmarkPage.classList.add('page--hidden');
+      createPage.classList.add('page--hidden');
+      profilePage.classList.add('page--hidden');
 
       // einblenden
-      currentIcon.classList.remove("icon--active");
+      currentIcon.classList.remove('icon--active');
       // if homeButton clicked, show home page. If bookmarks clicked, show bookmarks etc.
       console.log(navButton.dataset.page);
-      if (navButton.dataset.page === "bookmarksButton") {
-        bookmarkPage.classList.remove("page--hidden");
-      } else if (navButton.dataset.page === "homeButton") {
-        homePage.classList.remove("page--hidden");
-      } else if (navButton.dataset.page === "createButton") {
-        createPage.classList.remove("page--hidden");
-      } else if (navButton.dataset.page === "profileButton") {
-        profilePage.classList.remove("page--hidden");
+      if (navButton.dataset.page === 'bookmarksButton') {
+        bookmarkPage.classList.remove('page--hidden');
+      } else if (navButton.dataset.page === 'homeButton') {
+        homePage.classList.remove('page--hidden');
+      } else if (navButton.dataset.page === 'createButton') {
+        createPage.classList.remove('page--hidden');
+      } else if (navButton.dataset.page === 'profileButton') {
+        profilePage.classList.remove('page--hidden');
       }
     });
   });
